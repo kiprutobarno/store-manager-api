13b9954045c81b1e652e330239a3c6fc1030f555
Kipruto Barno <barxwells@gmail.com>
2018-10-20 12:07:35 +0300

    [Finishes #161360431] added jwt token-based endpoint protection security feature

--- app/api/v1/products.py
+++ app/api/v1/products.py
@@ -1,26 +1,32 @@
 from flask_restful import Resource
+from flask_jwt import jwt_required
 
 
 class ShowAllProducts(Resource):
+    @jwt_required()
     def get(self):
         return '', 204
 
 
 class ShowSingleProduct(Resource):
+    @jwt_required()
     def get(self, product_id):
         return '', 204
 
 
 class AddProduct(Resource):
+    @jwt_required()
     def post(self):
         return '', 204
 
 
 class UpdateProduct(Resource):
+    @jwt_required()
     def put(self, product_id):
         return '', 204
 
 
 class DeleteProduct(Resource):
+    @jwt_required()
     def delete(self, product_id):
         return '', 204
