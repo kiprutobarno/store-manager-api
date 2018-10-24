92558c8bf748016b59156beb5fc2a6472b6536fc
Kipruto Barno <barxwells@gmail.com>
2018-10-24 12:23:24 +0300

    [Chore #161393899] implemented object oriented programming on appliaction models

---
+++ app/tests/v1/test_application.py
@@ -0,0 +1,24 @@
+import unittest
+from flask import json, current_app
+from app import create_app
+
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def teardown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config['TESTING'])
+
+
+if __name__ == '__main__':
+    unittest.main()
